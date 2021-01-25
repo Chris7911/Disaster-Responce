953d1c35d71261e09ca2b642605dc129381b79f4
unknown <A40471@itri.org.tw>
2021-01-25 17:04:29 +0800

    Add machine learning pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,82 @@
 import sys
+import nltk
+nltk.download(['punkt', 'wordnet'])
+import pandas as pd
+from sqlalchemy import create_engine
+import re
+from nltk.tokenize import word_tokenize
+from stop_words import get_stop_words
+stop_words = get_stop_words('english')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+import pickle
 
 
 def load_data(database_filepath):
-    pass
+    # load data from database
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    table_name = database_filepath.split('.')[0]
+    df = pd.read_sql_table(table_name, engine)
+    X = df.message.values
+    Y = df.iloc[:, 4:].to_numpy()
+    category_names = list(df.columns[4:])
+
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    # normalize case and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    # tokenize text
+    tokens = word_tokenize(text)
+    
+    # remove stop words and lemmatize tokens
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+    
+    return tokens
 
 
 def build_model():
-    pass
+    # build a machine learning pipeline
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)), 
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier(random_state=1), n_jobs=-1))
+        ])
+    
+    # find a better model using grid search
+    parameters = {
+        'clf__estimator__n_estimators': [100, 150], 
+        'clf__estimator__warm_start': [True, False]
+        }
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    # test the model using classification report with precision, recall, and f1-score
+    Y_pred = model.predict(X_test)
+    for i, col_name in enumerate(category_names):
+        print('{}'.format(col_name))
+        print(classification_report(Y_test[:, i], Y_pred[:, i]))
+
+    # show the accuracy as well
+    print('Accuracy is {}'.format((Y_test == Y_pred).mean()))
 
 
 def save_model(model, model_filepath):
-    pass
+    # save the model as a pickle file
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
 
 
 def main():
